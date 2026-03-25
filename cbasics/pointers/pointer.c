56d75802eaec4fef721e3175e579aba03cd5b4f7
rkravikantkr <rkravikantkr@gmail.com>
2026-03-25 18:31:15 +0530

    cbasics/pointers/pointer.c pointer arithmetic

--- cbasics/pointers/pointer.c
+++ cbasics/pointers/pointer.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+#include <inttypes.h>
 #include <stdio.h>
 /*
  * bytes allocation based on data type
@@ -12,12 +14,47 @@
  * int *p = &a;  // p is a pointer to integer; it stores address of a integer
  * variable; &a gets address of a *p ; // dereferencing the pointer variable,
  * gives value at address of a.
+ *
+ * char c = 'A';
+ * char *ptc = &c;
+ *
  */
 
-int main(void) {
-    int a;
-    int *p = &a;
+void pointerfn() {
+    // binary representation of 1025 stored in a
+    int a = 1025; //[[00000000][00000000][00000100][00000001]]
 
-    printf("%p\n", &a); // print address of a
-    printf("%p\n", p);  // p contains same address as value stored in p
+    int *p;
+    p = &a;
+
+    assert(p == &a);
+
+    // *p dereferancing pointer, getting the value stored at that address.
+    assert(*p == a); //  value stored at address of a, which is stored in p.
+
+    // making changes to value stored in a via a pointer to it.
+    assert(*p + 10 == a + 10);
+
+    // printf("address of pointer to integer : %p\n", &p);
+
+    assert(sizeof(int) == 4 && "integer type has 4 bytes");
+
+    // printf("address of p+1: %p\n", p + 1);
+
+    char *pc;
+    pc = (char *)p; // typecasting pointer to integer to pointer to character.
+    assert(*pc == 1 && "dereferencing pointer to char reads only 1 byte");
+    // assert(*pc + 1 == 4 && "dereferencing pointer to char reads only 1
+    // byte");
+
+    printf("%" PRIuPTR, (uintptr_t)p);
+
+    printf("\n");
+    printf("%" PRIuPTR, (uintptr_t)p + 1);
+
+    printf("\n");
+    printf("%" PRIuPTR, (uintptr_t)pc);
+    printf("\n");
+    printf("%" PRIuPTR, (uintptr_t)pc + 1);
 }
+int main(void) { pointerfn(); }
