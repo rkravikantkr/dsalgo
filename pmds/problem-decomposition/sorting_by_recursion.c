0a997ea1f0a5ab7f77a1933e07bc40f600bae07d
rkravikantkr <rkravikantkr@gmail.com>
2026-04-10 16:51:02 +0530

    pmds: clear partioning method in quick sort

--- pmds/problem-decomposition/sorting_by_recursion.c
+++ pmds/problem-decomposition/sorting_by_recursion.c
@@ -9,13 +9,49 @@ void quick_sort(int *, int, int);
 int main() {
 
     // int arr[] = {1, 2, 3, 4, 5, 6};
-    int arr[] = {99, 3, 8, 7, 1, -1, -320, 0, -13};
+    int arr[] = {8, 7, 6, 5, 4, 3, 3, 3, 1, 2, 4, 6, 7, -9};
     int size = sizeof(arr) / sizeof(arr[0]);
     // merge_sort(arr, 0, size - 1);
     quick_sort(arr, 0, size - 1);
+    printf("\nsorted array\n");
     for (int l = 0; l <= size - 1; l++) {
         printf("%d ", arr[l]);
     }
+}
+
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int make_partition(int *arr, int low, int high) {
+
+    printf("before partitioning\n");
+    for (int i = 0; i <= high; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    int i = low - 1; // i tracks elements < pivot position
+    int pivot = arr[high];
+
+    for (int t = low; t < high; t++) {
+        if (arr[t] < pivot) {
+            swap(&arr[++i], &arr[t]);
+        }
+    }
+    swap(&arr[++i], &arr[high]);
+
+    printf("after partitioning\n");
+    for (int i = 0; i <= high; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\npartitioning index: %d", i);
+    printf("\n");
+    printf("\n");
+
+    return i;
 }
 
 int partition(int *arr, int left, int right) {
@@ -31,18 +67,31 @@ int partition(int *arr, int left, int right) {
             arr[i] = arr[j];
             arr[j] = temp;
         }
+        for (int l = 0; l <= j; l++) {
+            printf("%d ", arr[l]);
+        }
+
+        printf("\ni: %d - j: %d\n", i, j);
     }
-    if (arr[j] <= pivot)
+    if (arr[j] <= pivot) {
+        // printf("curr pivot index: %d, value: %d\n", j, pivot);
         return j;
-    else
+    } else {
+        // printf("curr pivot index: %d, value: %d\n", j - 1, pivot);
         return j - 1;
+    }
 }
 
 void quick_sort(int *arr, int i, int j) {
+    printf("qsort with i:%d, j:%d\n", i, j);
     if (i >= j)
         return;
-    int p = partition(arr, i, j);
-    quick_sort(arr, i, p);
+    // int p = partition(arr, i, j);
+    // quick_sort(arr, i, p);
+    // quick_sort(arr, p + 1, j);
+
+    int p = make_partition(arr, i, j);
+    quick_sort(arr, i, p - 1);
     quick_sort(arr, p + 1, j);
 }
 
